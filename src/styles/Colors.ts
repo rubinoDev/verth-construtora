9f1ea37817db8fe42703e0137eb822d7152e48e8
victor-rubino <rubinodev@gmail.com>
2023-07-17 19:54:48 -0300

    Setting colors and adding images

--- src/styles/Colors.ts
+++ src/styles/Colors.ts
@@ -1,12 +1,8 @@
 const Colors = {
-  color_01: '#E6EAED',
-  color_02: '#B4C1C9',
-  color_03: '#576E7B',
-  color_04: '#0A363E',
-  bg_color: '#DFD2C2',
-  text_color: '#132317',
-  button_bg: '#5AF1D6',
-  admin_button_bg: '#D9D9D9'
+  white: '#FAFAFA',
+  blue: '#0195FF',
+  black: '#000100',
+  green: '#8DD11C'
 };
 
 export default Colors;
